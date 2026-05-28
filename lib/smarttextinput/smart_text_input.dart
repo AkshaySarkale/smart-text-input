5c7798e6238c04a7a1361cdf5758eb74599f93a1
AkshaySarkale <akshaysarkale999@gmail.com>
2026-05-28 14:28:21 +0530

    changes in text feild

--- lib/smarttextinput/smart_text_input.dart
+++ lib/smarttextinput/smart_text_input.dart
@@ -1,140 +1,195 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class SmartTextInput extends StatelessWidget {
+class SmartTextInput extends StatefulWidget {
   final TextEditingController txtCtrl;
   final String? Function(String?)? validator;
   final AutovalidateMode? autovalidateMode;
   final bool isPassword;
   final bool isGmail;
-  final String hintText;
   final bool isMobNumber;
+  final bool isOtp;
+  final String hintText;
   final Color? hintTextCol;
   final bool isLabelText;
   final String labelText;
   final bool readOnly;
+  final bool enabled;
   final int defaultMobileLength;
+  final int otpDigit;
+  final int minimunPasswordlenght;
+  final bool showCounterText;
+  final bool showprefixIcon;
+  final IconData? prefixIcon;
   final bool showSuffixIcon;
   final IconData? suffixIcon;
   final VoidCallback? onSuffixTap;
-  final int otpDigit;
-  final bool isOtp;
-  final bool enabled;
-  final bool showprefixIcon;
-  final IconData? prefixIcon;
-  final int minimunPasswordlenght;
-  final bool showCounterText;
   final Color? focusBorderColor;
   final Color? errorBorderColor;
 
   const SmartTextInput({
     super.key,
     required this.txtCtrl,
-    this.isPassword = false,
     this.validator,
+    this.autovalidateMode,
+    this.isPassword = false,
     this.isGmail = false,
     this.isMobNumber = false,
+    this.isOtp = false,
     this.hintText = "",
     this.hintTextCol,
     this.isLabelText = false,
     this.labelText = "",
     this.readOnly = false,
+    this.enabled = true,
     this.defaultMobileLength = 10,
     this.otpDigit = 6,
-    this.isOtp = false,
-    this.showprefixIcon = false,
-    this.prefixIcon,
-    this.enabled = true,
     this.minimunPasswordlenght = 6,
     this.showCounterText = false,
-    this.focusBorderColor,
-    this.errorBorderColor,
-    this.autovalidateMode,
+    this.showprefixIcon = false,
+    this.prefixIcon,
     this.showSuffixIcon = false,
     this.suffixIcon,
     this.onSuffixTap,
+    this.focusBorderColor,
+    this.errorBorderColor,
   });
 
   @override
+  State<SmartTextInput> createState() => _SmartTextInputState();
+}
+
+class _SmartTextInputState extends State<SmartTextInput> {
+  bool isObscure = true;
+  @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: txtCtrl,
-      obscureText: isPassword ? true : false,
-      validator:
-          validator ??
-          (value) {
+      controller: widget.txtCtrl,
+      obscureText: widget.isPassword ? isObscure : false,
+      enabled: widget.enabled,
+      readOnly: widget.readOnly,
+      autovalidateMode: widget.autovalidateMode,
+      keyboardType: widget.isGmail ? TextInputType.emailAddress
+          : widget.isMobNumber ? TextInputType.number : TextInputType.text,
+
+      inputFormatters: widget.isMobNumber || widget.isOtp ?
+      [
+        FilteringTextInputFormatter.digitsOnly,
+      ] : null,
+
+      maxLength: widget.isMobNumber
+          ? widget.defaultMobileLength
+          : widget.isOtp
+          ? widget.otpDigit
+          : null,
+
+      validator: widget.validator ??
+              (value) {
             if (value == null || value.trim().isEmpty) {
               return "This field is required";
             }
-            if (isGmail) {
+            if (widget.isGmail) {
               if (!RegExp(
                 r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
               ).hasMatch(value)) {
-                return "Enter valid email";
+                return "Enter Valid Email";
               }
             }
-            if (isMobNumber) {
-              if (value.length != defaultMobileLength) {
+            if (widget.isMobNumber) {
+              if (value.length < widget.defaultMobileLength) {
                 return "Enter Valid Mobile Number";
               }
             }
-            if (isPassword) {
-              if (value.length != minimunPasswordlenght) {
-                return "Enter Valid Password";
+            if (widget.isPassword) {
+              if (value.length < widget.minimunPasswordlenght) {
+                return "Password Must Be ${widget.minimunPasswordlenght} Characters";
               }
             }
-            if (isOtp) {
-              if (value.length != otpDigit) {
-                return "Enter Valid Otp";
+            if (widget.isOtp) {
+              if (value.length < widget.otpDigit) {
+                return "Enter Valid OTP";
               }
             }
             return null;
           },
-      inputFormatters: isMobNumber || isOtp
-          ? [FilteringTextInputFormatter.digitsOnly]
-          : null,
-      autovalidateMode: autovalidateMode,
-      keyboardType: isGmail
-          ? TextInputType.emailAddress
-          : isMobNumber
-          ? TextInputType.number
-          : TextInputType.text,
-      readOnly: readOnly,
-      maxLength: isMobNumber
-          ? defaultMobileLength
-          : isOtp
-          ? otpDigit
-          : isPassword
-          ? minimunPasswordlenght
-          : null,
       decoration: InputDecoration(
-        suffixIcon: isPassword || suffixIcon != null
-            ? IconButton(
-          onPressed: onSuffixTap,
-          icon: Icon(suffixIcon),
-        )
-            : IconButton(
-          onPressed: onSuffixTap,
-          icon: Icon(Icons.remove_red_eye_outlined),
-        ),
-        counterText: showCounterText ? null : "",
-        prefixIcon: showprefixIcon && prefixIcon != null
-            ? Icon(prefixIcon)
-            : isGmail
+        counterText: widget.showCounterText ? null : "",
+        prefixIcon: widget.showprefixIcon && widget.prefixIcon != null
+            ? Icon(widget.prefixIcon)
+            : widget.isGmail
             ? const Icon(Icons.email_outlined)
-            : isMobNumber
+            : widget.isMobNumber
             ? const Icon(Icons.phone_outlined)
-            : isPassword
-            ? const Icon(Icons.password)
+            : widget.isPassword
+            ? const Icon(Icons.lock_outline)
             : null,
+
+        suffixIcon: widget.isPassword
+            ? IconButton(
+          onPressed: () {
+            setState(() {
+              isObscure = !isObscure;
+            });
+          },
+          icon: Icon(
+            isObscure
+                ? Icons.visibility_off
+                : Icons.visibility,
+          ),
+        )
+            : widget.showSuffixIcon && widget.suffixIcon != null
+            ? IconButton(
+          onPressed: widget.onSuffixTap,
+          icon: Icon(widget.suffixIcon),
+        )
+            : null,
+
         hintStyle: TextStyle(
-          color: hintTextCol ?? Colors.grey,
+          color: widget.hintTextCol ?? Colors.grey,
           fontStyle: FontStyle.italic,
         ),
+
+        labelText: widget.isLabelText
+            ? widget.labelText.isNotEmpty
+            ? widget.labelText
+            : widget.isPassword
+            ? "Password"
+            : widget.isMobNumber
+            ? "Mobile Number"
+            : widget.isGmail
+            ? "Email"
+            : null
+            : null,
+
+        hintText: widget.hintText.isNotEmpty ? widget.hintText
+            : widget.isPassword ? "Enter Password"
+            : widget.isMobNumber ? "Enter Mobile Number"
+            : widget.isGmail ? "Enter Email"
+            : widget.isOtp ? "Enter OTP"
+            : null,
+
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            color: Colors.grey,
+          ),
+        ),
+
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
           borderSide: BorderSide(
-            color: focusBorderColor ?? Colors.greenAccent,
+            color: widget.focusBorderColor ?? Colors.green,
+            width: 2,
+          ),
+        ),
+
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(
+            color: widget.errorBorderColor ?? Colors.red,
             width: 2,
           ),
         ),
@@ -142,32 +197,9 @@ class SmartTextInput extends StatelessWidget {
         focusedErrorBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
           borderSide: BorderSide(
-            color: errorBorderColor ?? Colors.red,
+            color: widget.errorBorderColor ?? Colors.red,
             width: 2,
           ),
-        ),
-        labelText: isLabelText
-            ? labelText.isNotEmpty
-                  ? labelText
-                  : isPassword
-                  ? "Password"
-                  : isMobNumber
-                  ? "Mobile Number"
-                  : isGmail
-                  ? "Gmail"
-                  : null
-            : null,
-        hintText: hintText.isNotEmpty
-            ? hintText
-            : isPassword
-            ? "Enter Password"
-            : isMobNumber
-            ? "Enter $defaultMobileLength Digit Mobile Number"
-            : isGmail
-            ? "Enter Gmail"
-            : null,
-        border: OutlineInputBorder(
-          borderSide: BorderSide(style: BorderStyle.solid),
         ),
       ),
     );
